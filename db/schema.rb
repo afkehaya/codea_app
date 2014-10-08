7a355d315fda60061c680f6c3a52c23f39489042
Alexander Kehaya <afkehaya@gmail.com>
2014-10-07 17:10:04 -0700

    customized profile page and deleted a few links.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004144233) do
+ActiveRecord::Schema.define(version: 20141006142223) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 20141004144233) do
     t.string   "food"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
